d72f1728fdafec2ed90830b49451ade74924085c
jk <fallafeljan@users.noreply.github.com>
2017-01-25 09:31:04 +0100

    simple args parsing, without optionals or usage

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-module.exports = function parseOptions(argv, options, usage = null) {
-  return {}
+module.exports = function parseOptions(argv, options, usage = undefined) {
+  let args = {}
+  
+  for (let i = 0; i < argv.length; i++) {
+    const keys = Object.keys(options)
+
+    for (let j = 0; j < keys.length; j++) {
+      const key = keys[j]
+      const alias = options[key].alias
+
+      if (argv[i] === `-${key}` || alias && argv[i] === `--${alias}`) {
+        const k = ++i
+        const val = argv[k]
+
+        if (argv.length <= k) {
+          break
+        } else {
+          args[key] = val
+
+          if (alias) {
+            args[alias] = val
+          }
+
+          break
+        }
+      }
+    }
+  }
+
+  return args
 }
